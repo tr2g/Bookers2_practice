da23515b278c084cc03452252c266945c757a5fe
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2023-03-14 07:18:17 +0000

    [setting]aboutページへのルーティンｇ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
 
   before_action :cofigure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_inpath_for(resource)
+    about_path
+  end
+
   protected
     def configure_permitted_orameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
