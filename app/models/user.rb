b0cca7fa13628a8535b2b466b74561bc048f7384
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2023-03-19 10:28:37 +0000

    [add]プロフィール画面を表示させる=>画像表示・ActiveStrageの復習

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
-  
+
   has_one_attached :profile_image
-  
-  def get_profile_image(width,height)
-    
-  
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image: 'no_image.jpg'
+  end
+
 end
