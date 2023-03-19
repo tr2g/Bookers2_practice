eacdd356d6411d0660485d064e2fa6f74fb8f182
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2023-03-19 09:10:22 +0000

    [edit]users_controller=>コントローラーで使うメソッド,記述方法などの復習

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,23 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user)
   end
 
   def index
   end
-  
+
   private
    def user_params
      params.require(:user).permit(:name, :profile_image, :introduction)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  has_one_attached :image
-         
   has_many :books, dependent: :destroy
+  
+  has_one_attached :profile_image
+  
+  def get_profile_image(width,height)
+    
   
 end
